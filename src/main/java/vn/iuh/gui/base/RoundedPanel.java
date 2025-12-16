13d5cd1ad13f5178305524143edc1a035dce269a
Duc Nguyen <nguyenvananhduc2005@gmail.com>
2025-12-16 16:30:34 +0700

    add icon

---
+++ src/main/java/vn/iuh/gui/base/RoundedPanel.java
@@ -0,0 +1,24 @@
+package vn.iuh.gui.base;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class RoundedPanel extends JPanel {
+    private int radius = 10;
+
+    public RoundedPanel(int radius) {
+        this.radius = radius;
+        setOpaque(false);
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        Graphics2D g2 = (Graphics2D) g.create();
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+                RenderingHints.VALUE_ANTIALIAS_ON);
+        g2.setColor(getBackground());
+        g2.fillRoundRect(0, 0, getWidth(), getHeight(), radius, radius);
+        g2.dispose();
+        super.paintComponent(g);
+    }
+}
